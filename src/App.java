cdb0d0c59bb52896c0b9874f750f5ca735163df0
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-09-13 10:36:44 +0200

    indexOf letar efter tecken

--- src/App.java
+++ src/App.java
@@ -4,8 +4,11 @@ public class App {
         Scanner tangentbord = new Scanner(System.in);
         System.out.print("Skriv in en text: ");
         String text1 = tangentbord.nextLine();
-        System.out.print("\""+text1+"\" har " +text1.length()+" bokstäver");   
-    
+        System.out.println("\""+text1+"\" har " +text1.length()+" bokstäver");   
+        System.out.print("Tecknet du letar efter: ");
+        String tecken = tangentbord.nextLine();
+        int plats = text1.indexOf(tecken);
+        System.out.println("Tecknet finns på den "+(plats+1)+" platsen");
     tangentbord.close();
     }
 }
